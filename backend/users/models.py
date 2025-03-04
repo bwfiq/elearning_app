5308e988a5be2014eb4414eb12e31eed9e2b506f
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-04 17:07:33 +0800

    feat(backend): add initial migration for users

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField("Username", max_length=240)
+    fullName = models.CharField("Full Name", max_length=240)
+    email = models.EmailField()
+    registrationDate = models.DateField("Registration Date", auto_now_add=True)
+    # TODO: Add following fields:
+    # isTeacher
+    # profilePicture
+    # password
+
+    def __str__(self):
+        return self.username
