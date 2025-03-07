f9300ed37569bda995101b3f1ec0c3d3ba48dd19
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-08 03:23:38 +0800

    feat: update user serializer to include all fields

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -6,4 +6,13 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("pk", "username", "fullName", "email", "registrationDate")
+        fields = ("pk", "username", "full_name", "email", "is_teacher", "registration_date", "password", "profile_picture")
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
