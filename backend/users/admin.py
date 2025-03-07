7313ab0d7f8de883538c7f4951d1bcbe88a49abc
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-08 04:30:55 +0800

    feat: add user admin view

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'full_name', 'is_staff', 'is_teacher', 'is_active')  # Customize the displayed fields
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('full_name', 'email', 'profile_picture')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'is_teacher', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'full_name', 'password', 'password2', 'is_teacher')}
+        ),
+    )
+    ordering = ('username',)
+admin.site.register(User, CustomUserAdmin)
