eaf09ecb261e89df24ab3b45c8e731bb6b826047
Mohammad Rafiq <mohammadrafiq567@gmail.com>
2025-03-09 14:10:58 +0800

    feat: add profile pic to home page

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, re_path, include
 from users import views
@@ -22,6 +5,8 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -31,4 +16,4 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/register/', views.register_user, name='register_user'),
     path('api/users/<int:user_id>/status_updates/', views.user_status_updates, name='user_status_updates'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
